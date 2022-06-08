f9200f9d0a9598abdcba255f9cfb0d8e1052c913
c3r38r170 <c3r38r170@gmail.com>
2022-06-08 17:21:09 -0300

    Edición de usuarios.

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   title = 'node-express-angular';
   status = 'DOWN';
   usuarios: Usuario[]=<any>[];
+  editando=false;
 
   constructor(
     private estadoServidor: ServidorService
@@ -39,12 +40,21 @@ export class AppComponent implements OnInit {
     // TODO investigar si esto puede ser más feo
     let u: Usuario=((Object.fromEntries(new FormData(e.target as HTMLFormElement))) as unknown) as Usuario;
     
-    this.usuarioService
-      .create(u)
-      .subscribe((result: any) => {
-        // TODO handle errors
-        this.usuarios.push(result as Usuario);
-      })
+    if(this.editando){
+      this.usuarioService
+        .edit(u)
+        .subscribe((result: any) => {
+          // TODO handle errors
+          this.usuarios.splice(this.usuarios.findIndex(uv=>uv.ID==u.ID),1,u)
+        })
+    }else{
+      this.usuarioService
+        .create(u)
+        .subscribe((result: any) => {
+          // TODO handle errors
+          this.usuarios.push(result as Usuario);
+        })
+    }
   }
 
   eliminarUsuario() {
@@ -57,6 +67,23 @@ export class AppComponent implements OnInit {
           // TODO handle errors
           this.usuarios.splice(this.usuarios.findIndex(u=>u.ID==IDUsuario),1)
         })
+  }
+
+  editarUsuario() {
+    let IDUsuario:number=Number.parseInt((document.getElementById('usuarios-lista') as HTMLInputElement)?.value);
+    let u = this.usuarios.find(u=>u.ID==IDUsuario);
+    for(let prop in u) {
+      // console.log(prop);
+      let input=document.getElementsByName(prop);
+      if(input.length)
+        ( input[0] as HTMLInputElement).value=(u as any)[prop];
+    }
+    this.editando=true;
+  }
+
+  cancelarEdicion(){
+    (document.getElementById('usuarios-formulario') as HTMLFormElement).reset();
+    this.editando=false;
   }
 
 }
