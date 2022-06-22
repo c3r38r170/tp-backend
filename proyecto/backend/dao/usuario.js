4d023c1fc288fdc1ca4a2c5c9ffe152f383504a5
c3r38r170 <c3r38r170@gmail.com>
2022-06-22 17:45:50 -0300

    Edición de la cantidad de tokens.

--- proyecto/backend/dao/usuario.js
+++ proyecto/backend/dao/usuario.js
@@ -39,19 +39,48 @@ function findAll({incluirContrasenia=false,incluirHabilitado=false,incluirTokens
 }
 
 function findById(id) {
-    return Usuario.findByPk(id,{include:['tokens']});
+    return Usuario.findByPk(id,{include:['tokensAsociadas']});
 }
 
 function deleteById(id) {
     return Usuario.destroy({ where: { id } });
 }
 
-function create(usuario) {
+async function create(usuario) {
     var newUsuario = new Usuario(usuario);
     return newUsuario.save();
 }
 
-function updateUsuario(usuario, id) {
+async function updateUsuario(usuario, id) {
+    let oldUsuario=await findById(id);
+    // console.log(oldUsuario);
+    let diferencia=usuario.tokens-oldUsuario.tokensAsociadas.length;
+    if(diferencia>0){
+        await aniadirTokens(oldUsuario,diferencia);
+    }else if(diferencia<0){
+        await quitarTokens(oldUsuario,-diferencia);
+    } 
     return Usuario.update(usuario, { where: { id } });
 }
+
+async function aniadirTokens(usuario,cantidad){
+    if(!cantidad)
+        return;
+
+    return Token.bulkCreate(new Array(cantidad).fill(new Token()))
+    .then(nuevasTokens=>{
+        usuario.setTokensAsociadas(nuevasTokens);
+    });
+}
+
+async function quitarTokens(usuario,cantidad){
+    if(!cantidad)
+        return;
+
+    let tokensAsociadas=await usuario.getTokensAsociadas();
+    for(let i=0;i<cantidad;i++)
+        tokensAsociadas[i].destroy();
+    // await usuario.removeTokensAsociadas(tokensAsociadas.splice(0,cantidad));
+}
+
 module.exports = usuarioDao;
