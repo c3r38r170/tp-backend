f310de12526609fe85524484b1ffa0f8d77817d1
c3r38r170 <c3r38r170@gmail.com>
2022-07-27 16:30:46 -0300

    Bloqueo y posible reinicio de formulario al eliminar usuario.

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   status = 'DOWN';
   usuarios: Usuario[]=<any>[];
   editando=false;
+  esperando=false;
 
   constructor(
     private estadoServidor: ServidorService
@@ -31,6 +32,10 @@ export class AppComponent implements OnInit {
       .subscribe((result: any)=>{
         this.usuarios=result;
       })
+  }
+
+  obtenerUsuarioActual():number{
+    return Number.parseInt((document.getElementById('usuarios-lista') as HTMLInputElement)?.value)
   }
 
   enviarUsuario(e:Event) {
@@ -58,19 +63,25 @@ export class AppComponent implements OnInit {
   }
 
   eliminarUsuario() {
-    let IDUsuario:number=Number.parseInt((document.getElementById('usuarios-lista') as HTMLInputElement)?.value);
+    let IDUsuario:number=this.obtenerUsuarioActual();
     
-    if(IDUsuario)
+    if(IDUsuario){
+      this.esperando=true;
+
       this.usuarioService
         .delete(IDUsuario)
         .subscribe((result: any)=>{
           // TODO handle errors
-          this.usuarios.splice(this.usuarios.findIndex(u=>u.ID==IDUsuario),1)
+          this.usuarios.splice(this.usuarios.findIndex(u=>u.ID==IDUsuario),1);
+          this.esperando=false;
+          if(this.editando) // La eliminación usa el mismo campo que la edición.
+            this.cancelarEdicion();
         })
+    }
   }
 
   editarUsuario() {
-    let IDUsuario:number=Number.parseInt((document.getElementById('usuarios-lista') as HTMLInputElement)?.value);
+    let IDUsuario:number=this.obtenerUsuarioActual();
     let u = this.usuarios.find(u=>u.ID==IDUsuario);
     for(let prop in u) {
       let input=document.getElementsByName(prop);
